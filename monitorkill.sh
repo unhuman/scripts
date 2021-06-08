b8719303725156f5c8d1e5335ee640e903f43cf1
Uman, Howard <human@cvent.com>
2021-06-08 18:11:28 -0400

    Improve output

--- monitorkill.sh
+++ monitorkill.sh
@@ -8,15 +8,15 @@ process_name=${pname}
 max_cpu=${maxcpu}
 
 usage() { 
-   echo "${1}"
-   echo "This will monitor the processes by name and kill processes that are using more than the CPU allowed"
-   echo "This can be used in a cron job to periodically monitor troublesome processes"
-   echo 
+   echo "${1}" 1>&2
+   echo "This will monitor the processes by name and kill processes that are using more than the CPU allowed" 1>&2
+   echo "This can be used in a cron job to periodically monitor troublesome processes" 1>&2
+   echo  1>&2
    echo "Usage: ${1} -p processName -c maxCPU [-m y|N] [-k seconds]" 1>&2
-   echo "       -p processName is the name (or part of the name) of the process you want to monitor."
-   echo "       -c maxCPU is the amount of CPU to tolerate.  If it goes over, kill will be attepted."
-   echo "       -m multiple tolerates > 1 processes that match the processName."
-   echo "       -k k9Delay is time, in seconds, that the kill attempt should be watched.  kill -9 attempted after."
+   echo "       -p processName is the name (or part of the name) of the process you want to monitor." 1>&2
+   echo "       -c maxCPU is the amount of CPU to tolerate.  If it goes over, kill will be attepted." 1>&2
+   echo "       -m multiple tolerates > 1 processes that match the processName." 1>&2
+   echo "       -k k9Delay is time, in seconds, that the kill attempt should be watched.  kill -9 attempted after." 1>&2
    exit 1
 }
 
@@ -50,24 +50,24 @@ fi
 processInfo=`ps -Ao pid,pcpu,utime,command | grep ${processName} | grep -v ${0} | grep -v grep`
 count=`echo ${processInfo} | wc -l | tr -d ' '`
 if [[ ${count} = 0 || ${processInfo} == '' ]]; then
-   echo "Notice: No process ${processName} found." 
+   echo "Notice: No process ${processName} found." 1>&2
    exit 0
 elif [[ ${multi} = false && ${count} -gt 1 ]]; then
-   echo "Error: Multiple ${processName} (${count}) found."
+   echo "Error: Multiple ${processName} (${count}) found." 1>&2
    exit -2
 fi 
 
 echo "${processInfo}" | while IFS= read -r line ; do
    cpu=`echo ${line} | awk '{print $2}'`
    if [[ $cpu -gt maxCPU ]]; then
-      echo "Notice: Killing ${processName} because CPU ${cpu} > ${maxCPU}"
+      echo "Notice: Killing ${processName} because CPU ${cpu} > ${maxCPU}" 1>&2
       pid=`echo ${line} | awk '{print $1}'`
       kill ${pid}
 
       if [[ $k9Delay -gt 0 ]]; then 
          kill9=false
          for i in {1..${k9Delay}}; do
-            echo "Monitor count: ${i}/${k9Delay}"
+            # echo "Monitor count: ${i}/${k9Delay}" 1>&2
             sleep 1
             current=`ps -p ${pid} | grep ${processName}`
             if [[ ${current} == "" ]]; then
@@ -76,7 +76,7 @@ echo "${processInfo}" | while IFS= read -r line ; do
             fi
          done
          if [[ ${pid} != "" ]]; then
-            echo "Notice: Kill -9 ${pid} ${processName} because it did not die"
+            echo "Notice: Kill -9 ${pid} ${processName} because it did not die" 1>&2
             kill -9 ${pid}
          fi
       fi
